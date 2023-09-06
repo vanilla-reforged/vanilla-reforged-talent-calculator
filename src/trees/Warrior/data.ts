16fd57f1ac592a95ea31e8dcc72a564b685d9da1
vanilla-reforged <vanilla.reforged@outlook.com>
2023-09-06 13:45:46 +0000

    update

--- src/trees/Warrior/data.ts
+++ src/trees/Warrior/data.ts
@@ -117,7 +117,7 @@ export const data: TalentData = {
         icon: icons["ability_backstab"],
         maxRank: 5,
         reqPoints: 15,
-        description: talentText`Increases the critical strike damage bonus of your Bloodthirst, Cleave, Execute, Heroic Strike, Mortal Strike and Overpower abilities by  ${[
+        description: talentText`Increases the critical strike damage bonus of your Bloodthirst, Cleave, Execute, Heroic Strike, Mortal Blow, Mortal Strike and Overpower abilities by  ${[
           6,
           12,
           18,
@@ -167,7 +167,7 @@ export const data: TalentData = {
         reqPoints: 30,
         prereq: "Sweeping Strikes",
         arrows: [{ dir: "down", from: "e2", to: "g2" }],
-        description: talentText`A vicious strike that deals weapon damage plus 85. (6 sec cooldown)`,
+        description: talentText`A vicious strike that deals weapon damage plus 85 and wounds the target, reducing the effectiveness of any healing by 50% for 10 sec. (6 sec cooldown)`,
       },
     },
   },
