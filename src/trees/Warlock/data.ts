6e8f776304d9ecc62ebcc4190d85a09f504ccb79
vanilla-reforged <vanilla.reforged@outlook.com>
2023-09-29 11:13:43 +0000

    update

--- src/trees/Warlock/data.ts
+++ src/trees/Warlock/data.ts
@@ -47,7 +47,7 @@ export const data: TalentData = {
           0.1,
           0.2,
           0.3,
-        ]}sec.`,
+        ]} sec.`,
       },
       "Improved Corruption": {
         name: "Improved Corruption",
