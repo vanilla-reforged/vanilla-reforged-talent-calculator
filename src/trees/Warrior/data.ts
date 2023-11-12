920ebab449911105cfeaf677fce647c8d607d8a4
vanilla-reforged <vanilla.reforged@outlook.com>
2023-11-12 18:38:13 +0000

    update

--- src/trees/Warrior/data.ts
+++ src/trees/Warrior/data.ts
@@ -206,7 +206,7 @@ export const data: TalentData = {
         icon: icons["ability_rogue_eviscerate"],
         maxRank: 5,
         reqPoints: 0,
-        description: talentText`Increases your chance to get a critical strike with melee weapons by ${[
+        description: talentText`Improves your chance to get a critical strike by ${[
           1,
           2,
           3,
