3445d8abc051b09f46fa83fd88c9e5cb5eea0aad
vanilla-reforged <vanilla.reforged@outlook.com>
2024-04-12 13:23:25 +0000

    update

--- src/trees/Warlock/data.ts
+++ src/trees/Warlock/data.ts
@@ -43,7 +43,7 @@ export const data: TalentData = {
         icon: icons["spell_shadow_gathershadows"],
         maxRank: 5,
         reqPoints: 0,
-        description: talentText`Gives your Affliction damage spells a ${[
+        description: talentText`Gives your Shadow spells a ${[
           2,
           4,
           6,
