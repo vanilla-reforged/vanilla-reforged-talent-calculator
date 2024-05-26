43763f3bb83ce077a8c2939b038197b97c51f894
vanilla-reforged <vanilla.reforged@outlook.com>
2024-05-26 12:16:33 +0000

    update

--- src/trees/Hunter/data.ts
+++ src/trees/Hunter/data.ts
@@ -280,11 +280,11 @@ export const data: TalentData = {
         maxRank: 5,
         reqPoints: 5,
         description: talentText`Reduces the cooldown of your Quick Shot ability by ${[
-          0.5,
-          1,
-          1.5,
+          0.4,
+          0.8,
+          1.2,
+          1.6,
           2,
-          2.5,
         ]} sec.`,
       },
       "Improved Aspect of the Hawk": {
